da6134454795fc307ca11ed97621d3be20765ddb
Gabriel Victor <122997003+GabrielHidaN@users.noreply.github.com>
2024-08-07 19:45:27 -0300

    add: container pages

--- script/index.js
+++ script/index.js
@@ -1,4 +1,12 @@
-function modificar(){
-  document.getElementById('teste').className = 'estilo-2'
-}
+var prox_cor = 'dark'
+function mudarcores(){
+  if (prox_cor == 'dark'){
+      document.getElementById('container').className = 'estilo-2'
+      prox_cor = 'light'
+  }
+  else{
+      document.getElementById('container').className = 'estilo-1'
+      prox_cor = 'dark'
+  }
 
+}
